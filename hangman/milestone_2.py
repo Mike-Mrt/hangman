3252377cecccc4a53cbb83b2b610221eb731ab64
Mike <mike@github.com>
2022-10-30 15:15:40 +0000

    Creating a word list and printing this list

---
+++ hangman/milestone_2.py
@@ -0,0 +1,6 @@
+# List containing the names of your 5 favourite fruits
+['strawberry', 'banana', 'pineapple', 'watermelon', 'apple']
+# Assigning list above to a variable
+word_list = ['strawberry', 'banana', 'pineapple', 'watermelon', 'apple']
+# Printing out newly created list
+print(word_list)
