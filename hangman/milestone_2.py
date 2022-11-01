4558d0fc380906aa6d8f7d734fc39d20aaeb9d11
Mike <mike@github.com>
2022-11-01 20:32:45 +0000

    Streamlining code - deleting few unecesary lines

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,7 +1,6 @@
 # Importing the random module:
 import random
-# List containing the names of your 5 favourite fruits
-['strawberry', 'banana', 'pineapple', 'watermelon', 'apple']
+
 # Assigning list above to a variable
 word_list = ['strawberry', 'banana', 'pineapple', 'watermelon', 'apple']
 # Printing out newly created list
