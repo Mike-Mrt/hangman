a80bc20f513302969e77ad95cbc232f16e55ba23
Mike <mike@github.com>
2022-11-01 20:30:42 +0000

    Adding random word generator

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,6 +1,12 @@
+# Importing the random module:
+import random
 # List containing the names of your 5 favourite fruits
 ['strawberry', 'banana', 'pineapple', 'watermelon', 'apple']
 # Assigning list above to a variable
 word_list = ['strawberry', 'banana', 'pineapple', 'watermelon', 'apple']
 # Printing out newly created list
 print(word_list)
+
+# Assigning randomly generated word into word variable and print:
+word = random.choice(word_list)
+print(word)
