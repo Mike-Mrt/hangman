a47514a24836c268102d6a1b5e89864bea4fc1cc
Mike <mike@github.com>
2022-11-02 15:19:05 +0000

    Adding check for user input

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,5 +1,7 @@
 # Importing the random module:
 import random
+# Importing string module:
+import string
 
 # Assigning list above to a variable
 word_list = ['strawberry', 'banana', 'pineapple', 'watermelon', 'apple']
@@ -12,3 +14,9 @@ print(word)
 
 # Asking user to enter a single ketter and assigning to variable
 guess = input("Please guess a single letter: ")
+
+# Checking if length of guess is 1 and an alphabet (lower or upper)
+if len(guess) and guess in list(string.ascii_letters):
+    print("Good guess!")
+else:
+    print("Oops! Thats is not a valid input.")
