837a50257f970fdd93c0822571c48c21cf058efb
Mike <mike@github.com>
2022-11-09 11:40:00 +0000

    Modifying the check_guess method which adds letter to the word_guessed list if guess is right

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -11,12 +11,19 @@ class Hangman:
         self.word_guessed = ['_']*len(self.word)
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
-
+    
+    # Method ot check if guess is in the word and to populate word_guessed if so otherwise remove life
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            for i,letter in enumerate(self.word):
+                if guess == letter:
+                    self.word_guessed[i] = letter
+                    return self.word_guessed
+        self.num_letters -= 1
 
+    # Method which asks the user for input (a letter) continuously until the while loop is broken
     def ask_for_input(self):
         while True:
             guess = input("Please guess a letter that you think is in the word: ")
@@ -32,11 +39,12 @@ class Hangman:
 
 
 # testing the initialisation of attributes: 
-# test1 = Hangman(['apple','pineapple','orange'], 5)
+test1 = Hangman(['apple','pineapple','orange'], 5)
 # print(test1.word_list)
 # print(test1.num_lives)
-# print(test1.word)
+print(test1.word)
 # print(test1.word_guessed)
 # print(test1.num_letters)
-# print(test1.list_of_guesses)
-# test1.ask_for_input()
+print(test1.list_of_guesses)
+print(test1.num_letters)
+test1.ask_for_input()
